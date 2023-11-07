7e85bd70b7af159dd4354bad1d864ff57c1753f4
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2023-11-07 19:51:21 +0700

    halaman 1

---
+++ app/src/main/java/com/example/pamlayout/Data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.pamlayout.Data
+
+import com.example.pamlayout.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.vanilla,
+        R.string.taro,
+        R.string.natural,
+        R.string.milk,
+        R.string.Lyche,
+    )
+}
